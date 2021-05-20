984e4caf26723a2e5c270681f49816f289a7d89f
AnuragSunilKumar <anuragsunilkumar1005@gmail.com>
2021-05-20 19:24:58 +0530

    shows a tied game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Board from './Components/Board';
 import History from './Components/History';
+import StatusMessage from './Components/StatusMessage';
 import { calculateWinner } from './Helpers';
 
 import './styles/root.scss';
@@ -13,9 +14,6 @@ const App = () => {
   const current = history[currentMove];
 
   const winner = calculateWinner(current.board);
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
@@ -46,7 +44,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current} />
       <Board board={current.board} handleSquareClick={handleSquareClick} />
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
